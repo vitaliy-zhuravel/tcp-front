7d266ae42f0a4edd2b74b38fb157a290ba107484
Vitaliy Zhuravel <vitaliy.z@immersetech.co>
2024-02-13 18:10:46 +0200

    corrected env

--- src/service/tcp-service.js
+++ src/service/tcp-service.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
-    
+const API_URL= process.env.REACT_APP_API_URL
+console.log(API_URL)
 export const sendData = async (value) => {
     try {
-        const response = await axios.post('http://localhost:3333/api/tcp-data', {key: value.text, port: value.port, host: value.host});
+        const response = await axios.post(`${API_URL}tcp-data`, {key: value.text, port: value.port, host: value.host});
+        console.log(response.data)
         return response.data; 
     } catch (error) {
         console.error('There was a problem with the axios request:', error);
@@ -11,7 +13,7 @@ export const sendData = async (value) => {
 
 export const getConnectStatus = async () => {
     try {
-        const response = await axios.get('http://localhost:3333/api/tcp-status')
+        const response = await axios.get(`${API_URL}tcp-status`)
         return response.data
     } catch (error) {
         console.log(error)
@@ -20,7 +22,7 @@ export const getConnectStatus = async () => {
 
 export const disConnect = async () => {
     try {
-       const response = await axios.post('http://localhost:3333/api/tcp-disconnect') 
+       const response = await axios.post(`${API_URL}tcp-disconnect`) 
        return response.data
     } catch (error) {
         console.log(error)
