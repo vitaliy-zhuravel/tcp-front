d6f9d5b36e605d94ad325c348d841f6f026c9414
Vitaliy Zhuravel <vitaliy.z@immersetech.co>
2024-02-14 11:53:27 +0200

    created deploy.sh2

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # stop and remove container
+echo "Stop tcp-front container"
 docker stop tcp-front
+
+echo "Remove tcp-front container"
 docker rm tcp-front
 
 # build new Docker-image
+echo "Build new image tcp-front"
 docker build -t tcp-front .
 
 # start new contaimner
+echo "Start tcp-front container"
 docker run -p 3000:3000 -d --name tcp-front tcp-front
